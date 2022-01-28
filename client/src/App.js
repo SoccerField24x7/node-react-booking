7f3c9dec2b9e115c912f8be53e1dc8fab2166c5f
Jesse Quijano <jesse@quijano.net>
2022-01-28 12:37:46 -0800

    add a simple top navigation

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './booking/Home';
 import Register from './auth/Register';
 import Login from './auth/Login';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
