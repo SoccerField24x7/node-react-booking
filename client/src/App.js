a6f6f3c7ac7f15d98dae0fd643e6b9f23ffd65eb
Jesse Quijano <jesse@quijano.net>
2022-01-28 07:25:02 -0800

    upgrade react router to use current version

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './booking/Home';
 import Register from './auth/Register';
 import Login from './auth/Login';
@@ -7,11 +7,11 @@ import Login from './auth/Login';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   );
 }
