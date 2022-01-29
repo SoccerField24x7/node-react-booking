481a75fe643036b0d0609b467bfd6903c0c66474
Jesse Quijano <jesse@quijano.net>
2022-01-28 18:02:57 -0800

    add and wire up redux

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { useResolvedPath } from "react-router-dom";
 
 const Home = () => {
+    const state = useSelector((state) => state);
     return (
         <div className="container-fluid h1 p-5 text-center">
             Home Page
+            <br />
+            {JSON.stringify(state)}
         </div>
     )
 }
