99923333405c903f23de20e4ddc423722988bcee
Jesse Quijano <jesse@quijano.net>
2022-02-01 20:14:06 -0800

    add esm, refactor, separate routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/auth';
 
 const app = express();
 
-app.get('/api/:message', (req, res) => {
-    res.status(200).send(req.params.message);
-});
+// route middleware
+app.use('/api', router);
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
