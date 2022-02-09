17f32d5fe9525624532d0039622fe5d787f1e61a
Jesse Quijano <jesse@quijano.net>
2022-02-08 19:59:53 -0800

    Add .env support

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { readdirSync } from 'fs';
+require('dotenv').config();
 
 const app = express();
 
 // route middleware
 readdirSync('./routes').map((routes) => app.use('/api', require(`./routes/${routes}`)));
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
