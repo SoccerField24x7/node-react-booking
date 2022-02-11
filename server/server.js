2bc845542a884fc0055bfc3ada18bca5bd1ba1d9
Jesse Quijano <jesse@quijano.net>
2022-02-10 20:31:38 -0800

    add morgan middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { readdirSync } from 'fs';
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
+
+// middlewares
+app.use(morgan('dev'));
 
 // route middleware
 readdirSync('./routes').map((routes) => app.use('/api', require(`./routes/${routes}`)));
