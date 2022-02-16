24e4bf6bb8397ad2596ba31f4339c112de292bd1
Jesse Quijano <jesse@quijano.net>
2022-02-15 22:33:58 -0800

    add mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import { readdirSync } from 'fs';
 const morgan = require('morgan');
+import cors from 'cors';
+import mongoose from 'mongoose';
 require('dotenv').config();
 
 const app = express();
 
+//db connection
+mongoose.connect(process.env.DATABASE_URI, {
+        useNewUrlParser: true,
+        // useFindAndModify: false,
+        useUnifiedTopology: true,
+        // useCreateIndex: true
+    })
+    .then(() => console.log('DB Connected'))
+    .catch((err) => console.log('DB Connection Error: ', err));
+
 // middlewares
+app.use(cors());
 app.use(morgan('dev'));
 
 // route middleware
