c9ce771c55721ecb6e1bd5b4adeb07fa1717d05c
Jesse Quijano <jesse@quijano.net>
2022-02-16 20:04:59 -0800

    create registration form

---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const RegisterForm = ({ handleSubmit, name, setName, email, setEmail, password, setPassword }) => (
+    <form onSubmit={handleSubmit}>
+            <div className="form-group mb-3 mt-3">
+                <label className="form-label">Your Name</label>
+                <input type="text" className="form-control" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="form-group mb-3">
+                <label className="form-label">Email Address</label>
+                <input type="email" className="form-control" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className="form-group mb-3">
+                <label className="form-label">Password</label>
+                <input type="password" className="form-control" placeholder="Enter Email" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+);
+
+export default RegisterForm;
