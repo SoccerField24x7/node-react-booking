35745057b9e1fdbb18d820d1f682ec1d16499e93
Jesse Quijano <jesse@quijano.net>
2022-02-20 22:07:16 -0800

    implement user save

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,32 @@
-export const showMessage = (req, res) => {
-    res.status(200).send(req.params.message);
-};
+import User from '../models/user';
 
 export const register = async (req, res) => {
     console.log(req.body);
+
+    const { name, email, password } = req.body;
+
+    // Validation
+    if (!name) return res.status(400).send('Name is required.');
+    if (!password) return res.status(400).send('Password is required.');
+    if (password.length < 6) return res.status(400).send('Password length must be at least 6 characters.');
+
+    try {
+        const userExists = await User.findOne( { email }).exec();
+        if (userExists) return res.status(400).send('Email already in use.');
+    } catch (err) {
+        console.log('FETCH USER FAILED', err);
+        return res.status(500).send('Email validation failed.');
+    }
+
+    // Clear to save!
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        console.log('USER CREATED', user);
+        return res.json({ ok: true });
+    } catch (err) {
+        console.log('CREATE USER FAILED', err);
+        return res.status(400).send('Error')
+    }
 };
